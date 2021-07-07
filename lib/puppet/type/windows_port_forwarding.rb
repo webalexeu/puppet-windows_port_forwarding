5788b130e1edfddf2e8b8e00df667a3ac8aae1ff
Alexandre JARDON <28548335+webalexeu@users.noreply.github.com>
2021-07-07 13:39:27 +0200

    Fix syntax checking

--- lib/puppet/type/windows_port_forwarding.rb
+++ lib/puppet/type/windows_port_forwarding.rb
@@ -1,30 +1,30 @@
 require 'puppet/parameter/boolean'
 
 Puppet::Type.newtype(:windows_port_forwarding) do
-  @doc = "Manage Windows Port Forwarding with Puppet"
+  @doc = 'Manage Windows Port Forwarding with Puppet'
 
   ensurable do
-    desc "How to ensure this port forwarding (`present` or `absent`)"
+    desc 'How to ensure this port forwarding (`present` or `absent`)'
     defaultvalues
     defaultto(:present)
   end
 
   newparam(:listen_on, namevar: true) do
-    desc "Specifies the IP address and port for which to listen (ip:port)"
+    desc 'Specifies the IP address and port for which to listen (ip:port)'
     validate do |value|
-      fail("listen_on should be structured ip:port") if value !~ /^(.*):(.*)$/
+      raise("listen_on should be structured ip:port") if value !~ %r{/^(.*):(.*)$/}
     end
   end
 
   newproperty(:connect_on) do
-    desc "Specifies the IP address and port to which to connect (ip:port)"
+    desc 'Specifies the IP address and port to which to connect (ip:port)'
     validate do |value|
-      fail("listen_on should be structured ip:port") if value !~ /^(.*):(.*)$/
+      raise("listen_on should be structured ip:port") if value !~ %r{/^(.*):(.*)$/}
     end
   end
 
   newproperty(:protocol) do
-    desc "Protocol to establish proxy service (`v4tov4`/`v6tov6`/`v4tov6`/`v6tov4`)"
+    desc 'Protocol to establish proxy service (`v4tov4`/`v6tov6`/`v4tov6`/`v6tov4`)'
     newvalues(:v4tov4, :v6tov6, :v4tov6, :v6tov4)
     defaultto(:v4tov4)
   end
