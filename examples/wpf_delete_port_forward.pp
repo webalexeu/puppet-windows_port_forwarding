336a8bb72cf3edabf695a0fb5c089f73ad50a580
WebalexEU <28548335+webalexeu@users.noreply.github.com>
2025-01-02 17:26:56 +0100

    Code fix

--- examples/wpf_delete_port_forward.pp
+++ examples/wpf_delete_port_forward.pp
@@ -1,13 +1,13 @@
 # @PDQTestWin
-windows_port_forwarding{'port forward':
-    ensure     => absent,
-    protocol   => 'v4tov4',
-    listen_on  => '*:8990',
-    connect_on => "${$::ipaddress}:8988";
+windows_port_forwarding { 'port forward':
+  ensure     => absent,
+  protocol   => 'v4tov4',
+  listen_on  => '*:8990',
+  connect_on => "${facts['networking']['ip']}:8988";
 }
 
-windows_port_forwarding{"${$::ipaddress}:9090":
-    ensure     => absent,
-    protocol   => 'v4tov4',
-    connect_on => '*:8988';
+windows_port_forwarding { "${facts['networking']['ip']}:9090":
+  ensure     => absent,
+  protocol   => 'v4tov4',
+  connect_on => '*:8988';
 }
